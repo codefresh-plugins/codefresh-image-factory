e6b2a259a8876d6fb7c46bca9085af89151f66a2
olegz <olegz@codefresh.io>
2021-10-22 19:38:47 +0300

    CR-6109

--- config.js
+++ config.js
@@ -1,8 +1,38 @@
+const crypto = require('crypto');
+const image = process.env.IMAGE;
+const accountName = process.env.CF_ACCOUNT;
+
 module.exports = {
-    image: process.env.IMAGE,
+
+    image,
+
+    digest: crypto.createHmac('sha256', image).update(`Digest-${accountName}-${image}`).digest('hex'),
+    hash: crypto.createHash('sha1')
+        .update(`hash-${accountName}-${image}`)
+        .digest('hex')
+        .replace(/-/g, '_').toLowerCase(),
 
     apiToken: process.env.CF_API_KEY,
-    accountName: process.env.CF_ACCOUNT,
     host: process.env.CF_URL || 'https://g.codefresh.io',
+
+    buildId: process.env.BUILD_ID,
+    pipelineId: process.env.PIPELINE_ID,
+
+    inspectId: process.env.INSPECT_ID || crypto.createHmac('sha256', image).update(`Id-${accountName}-${image}`).digest('hex'),
+    inspectSize: Number(process.env.INSPECT_SIZE) || undefined,
+    inspectOs: process.env.INSPECT_OS,
+    inspectArc: process.env.INSPECT_ARC,
+
+    commitMsg: process.env.COMMIT_MSG,
+    commitUrl: process.env.COMMIT_URL,
+
+
+    dockerFile: process.env.DOCKERFILE,
+
+    branch: process.env.BRANCH,
+    sha: process.env.SHA,
+
+    gitRepoOwner: process.env.GIT_REPO_OWNER,
+    gitRepoName: process.env.GIT_REPO_NAME,
 };
 
--- index.js
+++ index.js
@@ -1,26 +1,137 @@
-const crypto = require('crypto');
 const chalk = require('chalk');
 const rp = require('request-promise');
-const { host, apiToken, image, accountName } = require('./config');
+const config = require('./config');
+const {host, apiToken, image} = config;
+const {parseFamiliarName} = require('@codefresh-io/docker-reference');
+const DIGEST_PREFIX_REGEX = /^sha256:/;
 
 Promise.resolve()
-    .then(() => rp({
-            method: 'POST',
-            uri: `${host}/api/images/external`,
-            body: {
-                Image: image,
-                Digest: crypto.createHmac('sha256', image).update(`Digest-${accountName}-${image}`).digest('hex'),
-                Id: crypto.createHmac('sha256', image).update(`Id-${accountName}-${image}`).digest('hex'),
-            },
-            headers: {
-                'Authorization': `Bearer ${apiToken}`
-            },
-            json: true
-        })
-    )
+    .then(getPreRequires)
+    .then(buildArgs)
+    .then(createImage)
     .then(() => console.log(chalk.green(`Image created successfully`)))
-    .then(()=>process.exit(0))
+    .then(() => process.exit(0))
     .catch(error => {
         console.log(chalk.red(error.message));
         process.exit(1);
     });
+
+async function getPreRequires() {
+    try {
+        const reference = parseFamiliarName(image);
+
+        return Promise.all([
+
+            config.buildId && rp({
+                method: 'GET',
+                uri: `${host}/api/builds/${config.buildId}`,
+                headers: _getAuthHeader(),
+                json: true
+            }).then(build => {
+                if (!build) {
+                    throw Error(`Can't find build with id "${config.buildId}"`);
+                }
+                return build;
+            }),
+
+            config.pipelineId && rp({
+                method: 'GET',
+                uri: `${host}/api/pipelines/${config.pipelineId}`,
+                headers: _getAuthHeader(),
+                json: true
+            }).then(pipeline => {
+                if (!pipeline) {
+                    throw Error(`Can't find pipeline with id "${config.pipelineId}"`);
+                }
+                return pipeline;
+            })
+
+        ]).then(([build, pipeline]) => ({
+            reference, build, pipeline,
+            imageName: reference.repository
+        }));
+
+    } catch (err) {
+        throw Error(`Image is incorrect: ${err}`);
+    }
+}
+
+function buildArgs({pipeline, build, imageName, reference}) {
+    const progressId = build ? build.progress_id : undefined;
+    const internalImageId = config.inspectId.replace(DIGEST_PREFIX_REGEX, '');
+
+    const imageInfo = {
+        pipeline: pipeline,
+        buildDetails: {
+            progressId,
+            id: config.buildId,
+            branch: config.branch,
+            sha: config.sha,
+            commit: config.commitMsg,
+            commitURL: config.commitUrl,
+        },
+        imageDetails: {
+            inspect: {
+                'Id': config.inspectId,
+                'Size': config.inspectSize,
+                'Os': config.inspectOs,
+                'Architecture': config.inspectArc,
+                Config: {
+                    Labels: {
+                        'io.codefresh.repo.owner': config.gitRepoOwner,
+                        'io.codefresh.repo.name': config.gitRepoName,
+                        'io.codefresh.repo.branch': config.branch,
+                        'io.codefresh.repo.commit.message': config.commitMsg,
+                        'io.codefresh.repo.commit.url': config.commitUrl,
+                        'io.codefresh.repo.sha': config.sha,
+                    }
+                }
+            },
+            imageName,
+            hash: config.hash,
+            imageDisplayName: imageName,
+            dockerFile: config.dockerFile
+
+        }
+    };
+
+    const digest = config.digest.replace(DIGEST_PREFIX_REGEX, '');
+    const repoDigest = `${reference.repositoryUrl}@sha256:${digest}`;
+
+    const registryInfo = {
+        query: {
+            internalImageId
+        },
+        imageDetails: {
+            internalImageId,
+            repoDigest,
+            imageName,
+        },
+        tagInfo: {
+            registry: reference.domain,
+            repository: reference.repository,
+            tag: reference.tag
+        }
+    };
+
+    return {imageInfo, registryInfo};
+}
+
+async function createImage({imageInfo, registryInfo}) {
+
+    return rp({
+        method: 'POST',
+        uri: `${host}/api/images/factory`,
+        body: {
+            imageInfo,
+            registryInfo
+        },
+        headers: _getAuthHeader(),
+        json: true
+    });
+}
+
+
+function _getAuthHeader() {
+    return {'Authorization': `Bearer ${apiToken}`};
+}
