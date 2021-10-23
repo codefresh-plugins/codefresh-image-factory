58753350ac90f521809b289d83ccef21eba205c7
olegz <olegz@codefresh.io>
2021-10-23 14:47:05 +0300

    CR-6109

--- config.js
+++ config.js
@@ -15,19 +15,19 @@ module.exports = {
     apiToken: process.env.CF_API_KEY,
     host: process.env.CF_URL || 'https://g.codefresh.io',
 
-    buildId: process.env.BUILD_ID,
-    pipelineId: process.env.PIPELINE_ID,
+    buildId: process.env.BUILD_ID || undefined,
+    pipelineId: process.env.PIPELINE_ID || undefined,
 
     inspectId: process.env.INSPECT_ID || crypto.createHmac('sha256', image).update(`Id-${accountName}-${image}`).digest('hex'),
     inspectSize: Number(process.env.INSPECT_SIZE) || undefined,
-    inspectOs: process.env.INSPECT_OS,
-    inspectArc: process.env.INSPECT_ARC,
+    inspectOs: process.env.INSPECT_OS || undefined,
+    inspectArc: process.env.INSPECT_ARC || undefined,
 
-    commitMsg: process.env.COMMIT_MSG,
-    commitUrl: process.env.COMMIT_URL,
+    commitMsg: process.env.COMMIT_MSG || undefined,
+    commitUrl: process.env.COMMIT_URL || undefined,
 
 
-    dockerFile: process.env.DOCKERFILE,
+    dockerFile: process.env.DOCKERFILE || undefined,
 
     branch: process.env.BRANCH,
     sha: process.env.SHA,
